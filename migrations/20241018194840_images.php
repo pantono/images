1563130b7ae09aadfa23b39044488af58ee4946d
Chris Burton <csburton@gmail.com>
2025-09-06 15:07:35 +0100

    Fixing foriegn key issues

--- migrations/20241018194840_images.php
+++ migrations/20241018194840_images.php
@@ -15,23 +15,24 @@ final class Images extends AbstractMigration
             ->create();
 
         $this->table('image')
-            ->addColumn('file_id', 'integer')
-            ->addForeignKey('file_id', 'stored_file', 'id')
+            ->addColumn('file_id', 'integer', ['signed' => false])
             ->addColumn('date_created', 'datetime')
             ->addColumn('deleted', 'boolean')
             ->addColumn('mime_type', 'string')
+            ->addForeignKey('file_id', 'stored_file', 'id')
             ->create();
 
         $this->table('image_history')
-            ->addColumn('image_id', 'integer')
+            ->addColumn('image_id', 'integer', ['signed' => false])
             ->addColumn('date', 'datetime')
             ->addColumn('user_id', 'integer')
             ->addColumn('entry', 'text')
+            ->addForeignKey('image_id', 'image', 'id')
             ->create();
 
         $this->table('image_size')
-            ->addColumn('image_id', 'integer')
-            ->addColumn('size_type_id', 'integer')
+            ->addColumn('image_id', 'integer', ['signed' => false])
+            ->addColumn('size_type_id', 'integer', ['signed' => false])
             ->addColumn('file_id', 'integer', ['null' => true])
             ->addColumn('date_created', 'datetime')
             ->addForeignKey('image_id', 'image', 'id')
